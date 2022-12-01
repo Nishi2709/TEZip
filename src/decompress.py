6100315182c3f5010f1212d61042cb27729500fe
Nishi2709 <105194719+Nishi2709@users.noreply.github.com>
2022-12-01 18:16:22 +0900

    Feature: Add sequential loop(decomp)

--- src/decompress.py
+++ src/decompress.py
@@ -115,6 +115,7 @@ def run(WEIGHTS_DIR, DATA_DIR, OUTPUT_DIR, GPU_FLAG, VERBOSE):
 	# 推論用にキーフレームにパディング
 	X_test_pad = data_padding(X_test)
 
+
 	# キーフレームの場所と何枚先を推論したかを復元
 	key_frame_check = []
 	for X_testcut in X_test_pad:
@@ -327,174 +328,189 @@ def run_save_memory(WEIGHTS_DIR, DATA_DIR, OUTPUT_DIR, GPU_FLAG, VERBOSE, IMAGE_
 		print("ERROR: No such file or directory:", os.path.join(DATA_DIR, "entropy.dat"))
 		exit()
 	
-	decompress_data = np.frombuffer(input_data, dtype='int16')
+	decompress_data_all = np.frombuffer(input_data, dtype='int16')
 
 	# warm up(PREPROCESS)の値を復元
-	warm_up = decompress_data[-1]
+	warm_up = decompress_data_all[-1]
 	warm_up = warm_up.astype('int16')
-	decompress_data = decompress_data[:-1]
+	decompress_data_all = decompress_data_all[:-1]
 
 	# 圧縮前のnumpyのshapeを復元
-	X_test_shape = decompress_data[-5:]
+	X_test_shape = decompress_data_all[-5:]
 	X_test_shape = X_test_shape.astype('int16')
-	decompress_data = decompress_data[:-5]
+	decompress_data_all = decompress_data_all[:-5]
 
 	X_test = X_test.reshape(X_test_shape)
 
 	X_test = X_test / 255
 
 	# 推論用にキーフレームにパディング
-	X_test_pad = data_padding(X_test)
+	X_test_pad_all = data_padding(X_test)
 
 	# キーフレームの場所と何枚先を推論したかを復元
 	key_frame_check = []
-	for X_testcut in X_test_pad:
+	for X_testcut in X_test_pad_all:
 		for idx in range(X_testcut.shape[0]):
 			if not np.all(X_testcut[idx] == 0):
 				key_frame_check.append(idx)
 
-	key_frame_check.append(X_test_pad.shape[1])
+	key_frame_check.append(X_test_pad_all.shape[1])
 
-	if test_model.input.shape[2] != X_test_pad.shape[2] or test_model.input.shape[3] != X_test_pad.shape[3]:
+	if test_model.input.shape[2] != X_test_pad_all.shape[2] or test_model.input.shape[3] != X_test_pad_all.shape[3]:
 		print("ERROR:keyframe size and model size do not match.")
 		print("model size: height ", test_model.input.shape[2] - 7, "～", test_model.input.shape[2], " width ", test_model.input.shape[3] - 7, "～", test_model.input.shape[3])
 		print("key frame size: height ", X_test.shape[2], " width ",  X_test.shape[3])
 		exit()
 
+	# 逐次処理準備
+	# -iで指定した画像枚数を最も近いキーフレームに置き換える(キーフレームで区切らないといけないため)
+	IMAGE_NUM_PER_TIME_NEW = np.abs(np.asfarray(key_frame_check) - IMAGE_NUM_PER_TIME).argmin()
 
-	# predict
-	result_list = []
-	key_idx = warm_up
-	X_test_one = X_test_pad[0, 0]
-	X_test_one = X_test_one[np.newaxis, np.newaxis, :, :, :]
-	warm_up_frame = test_model.predict(X_test_one, batch_size)
-	for _ in range(warm_up):
-		result_list.append(warm_up_frame)
-
-	for idx in range(warm_up, len(key_frame_check[warm_up:]) + warm_up - 1):
-		for predict_idx in range(key_frame_check[idx], key_frame_check[idx+1]):
-			if predict_idx == key_frame_check[idx]:
-				X_test_one = X_test_pad[0, predict_idx]
-				X_test_one = X_test_one[np.newaxis, np.newaxis, :, :, :]
-				X_test_tmp = np.zeros(X_test_one.shape)
-				X_test_one = np.hstack([X_test_one, X_test_tmp])
-				X_hat = test_model.predict(X_test_one, batch_size)
-
-				X_hat_predict_one = X_test_pad[0, predict_idx]
-				X_hat_predict_one = X_hat_predict_one[np.newaxis, np.newaxis, :, :, :]
-				result_list.append(X_hat_predict_one)
-			
-			elif predict_idx == key_frame_check[idx] + 1:
-				X_test_one = X_test_pad[0, predict_idx - 1]
-				X_test_one = X_test_one[np.newaxis, np.newaxis, :, :, :]
-				X_test_tmp = np.zeros(X_test_one.shape)
-				X_test_one = np.hstack([X_test_one, X_test_tmp])
-				X_hat = test_model.predict(X_test_one, batch_size)
-
-				X_hat_predict_one = X_hat[0, 1]
-				X_hat_predict_one = X_hat_predict_one[np.newaxis, np.newaxis, :, :, :]
-				result_list.append(X_hat_predict_one)
-
-			else:
-				X_test_one = result_list[-1]
-				X_test_tmp = np.zeros(X_test_one.shape)
-				X_test_one = np.hstack([X_test_one, X_test_tmp])
-				X_hat = test_model.predict(X_test_one, batch_size)
-
-				X_hat_predict_one = X_hat[0, 1]
-				X_hat_predict_one = X_hat_predict_one[np.newaxis, np.newaxis, :, :, :]
-				result_list.append(X_hat_predict_one)
-
-	# 推論結果を一つにまとめる
-	X_hat_flat = result_list[0]
-	for X_hat_np in result_list[1:]:
-		X_hat_flat = np.hstack([X_hat_flat, X_hat_np])
-
-	X_hat_flat[0, 0] = X_test_pad[0, 0]
-
-	# 推論結果からパディングを外す
-	X_hat_no_pad = X_hat_flat[:,:, :X_test.shape[2], :X_test.shape[3]]
-
-	# GPU無:numpy GPU有:cupyに設定
-	if GPU_FLAG:
-		# tensorflowが占有しているメモリを解放
-		cuda.select_device(0)
-		cuda.close()
-		import cupy as xp
-	else:
-		import numpy as xp
-
+	sequential_times = int(math.ceil(len(file_paths) / IMAGE_NUM_PER_TIME_NEW))
 	
-	start = time.time()
-	# エントロピー符号化のmapping tableを復元
-	table = []
-	table_len = decompress_data[-1]
+	# 逐次実行
+	for sequential in range(1,sequential_times+1):
 
-	if table_len == -1:
-		decompress_data = decompress_data[:-1]
-	else:
-		table_start = -table_len - 1
-		table_np = decompress_data[table_start:-1]
-		for num in table_np:
-			table.append(num)
+	# キーフレーム
+	X_test_pad = x_pad_all...
+	key_farme_check = ....
+	# entorpy
+	decompress_data = decompress_data_all...
 
-		table_xp = xp.array(table)
 
-		elapsed_time = time.time() - start
+		# predict
+		result_list = []
+		key_idx = warm_up
+		X_test_one = X_test_pad[0, 0]
+		X_test_one = X_test_one[np.newaxis, np.newaxis, :, :, :]
+		warm_up_frame = test_model.predict(X_test_one, batch_size)
+		for _ in range(warm_up):
+			result_list.append(warm_up_frame)
 
-		if VERBOSE: print ("table_create:{0}".format(elapsed_time) + "[sec]")
+		for idx in range(warm_up, len(key_frame_check[warm_up:]) + warm_up - 1):
+			for predict_idx in range(key_frame_check[idx], key_frame_check[idx+1]):
+				if predict_idx == key_frame_check[idx]:
+					X_test_one = X_test_pad[0, predict_idx]
+					X_test_one = X_test_one[np.newaxis, np.newaxis, :, :, :]
+					X_test_tmp = np.zeros(X_test_one.shape)
+					X_test_one = np.hstack([X_test_one, X_test_tmp])
+					X_hat = test_model.predict(X_test_one, batch_size)
 
-		# エントロピー符号化のmapping tableを配列から削除
-		decompress_data = decompress_data[:table_start]
+					X_hat_predict_one = X_test_pad[0, predict_idx]
+					X_hat_predict_one = X_hat_predict_one[np.newaxis, np.newaxis, :, :, :]
+					result_list.append(X_hat_predict_one)
+				
+				elif predict_idx == key_frame_check[idx] + 1:
+					X_test_one = X_test_pad[0, predict_idx - 1]
+					X_test_one = X_test_one[np.newaxis, np.newaxis, :, :, :]
+					X_test_tmp = np.zeros(X_test_one.shape)
+					X_test_one = np.hstack([X_test_one, X_test_tmp])
+					X_hat = test_model.predict(X_test_one, batch_size)
 
-		# GPUがあるならばcupyに変換
+					X_hat_predict_one = X_hat[0, 1]
+					X_hat_predict_one = X_hat_predict_one[np.newaxis, np.newaxis, :, :, :]
+					result_list.append(X_hat_predict_one)
+
+				else:
+					X_test_one = result_list[-1]
+					X_test_tmp = np.zeros(X_test_one.shape)
+					X_test_one = np.hstack([X_test_one, X_test_tmp])
+					X_hat = test_model.predict(X_test_one, batch_size)
+
+					X_hat_predict_one = X_hat[0, 1]
+					X_hat_predict_one = X_hat_predict_one[np.newaxis, np.newaxis, :, :, :]
+					result_list.append(X_hat_predict_one)
+
+		# 推論結果を一つにまとめる
+		X_hat_flat = result_list[0]
+		for X_hat_np in result_list[1:]:
+			X_hat_flat = np.hstack([X_hat_flat, X_hat_np])
+
+		X_hat_flat[0, 0] = X_test_pad[0, 0]
+
+		# 推論結果からパディングを外す
+		X_hat_no_pad = X_hat_flat[:,:, :X_test.shape[2], :X_test.shape[3]]
+
+		# GPU無:numpy GPU有:cupyに設定
 		if GPU_FLAG:
-			decompress_data = xp.asarray(decompress_data)
+			# tensorflowが占有しているメモリを解放
+			cuda.select_device(0)
+			cuda.close()
+			import cupy as xp
+		else:
+			import numpy as xp
+
+		
+		start = time.time()
+		# エントロピー符号化のmapping tableを復元
+		table = []
+		table_len = decompress_data[-1]
+
+		if table_len == -1:
+			decompress_data = decompress_data[:-1]
+		else:
+			table_start = -table_len - 1
+			table_np = decompress_data[table_start:-1]
+			for num in table_np:
+				table.append(num)
+
+			table_xp = xp.array(table)
+
+			elapsed_time = time.time() - start
+
+			if VERBOSE: print ("table_create:{0}".format(elapsed_time) + "[sec]")
+
+			# エントロピー符号化のmapping tableを配列から削除
+			decompress_data = decompress_data[:table_start]
+
+			# GPUがあるならばcupyに変換
+			if GPU_FLAG:
+				decompress_data = xp.asarray(decompress_data)
+
+			start = time.time()
+			# エントロピー符号化から復元
+			decompress_data = replacing_based_on_frequency(decompress_data, table_xp, xp)
+			
+			elapsed_time = time.time() - start
+
+			if VERBOSE: print ("replacing_based_on_frequency:{0}".format(elapsed_time) + "[sec]")
+
+			# エントロピー符号化のテーブル作成のために1600との差分として保存したものの復元
+			decompress_data = xp.subtract(1600, decompress_data)
 
 		start = time.time()
-		# エントロピー符号化から復元
-		decompress_data = replacing_based_on_frequency(decompress_data, table_xp, xp)
+		# Density-based Spatial Encoding
+		tmp = xp.reshape(decompress_data, X_test_shape)
 		
+		# cupyに変換していたらnumpyに戻す
+		if GPU_FLAG:
+			tmp = xp.asnumpy(tmp)
+		difference_first = finding_difference(tmp)
+
 		elapsed_time = time.time() - start
 
-		if VERBOSE: print ("replacing_based_on_frequency:{0}".format(elapsed_time) + "[sec]")
+		if VERBOSE: print ("finding_difference:{0}".format(elapsed_time) + "[sec]")
 
-		# エントロピー符号化のテーブル作成のために1600との差分として保存したものの復元
-		decompress_data = xp.subtract(1600, decompress_data)
+		# 画像の出力
+		decompress = X_hat_no_pad * 255
+		decompress = decompress - difference_first
 
-	start = time.time()
-	# Density-based Spatial Encoding
-	tmp = xp.reshape(decompress_data, X_test_shape)
-	
-	# cupyに変換していたらnumpyに戻す
-	if GPU_FLAG:
-		tmp = xp.asnumpy(tmp)
-	difference_first = finding_difference(tmp)
+		decompress = np.where(decompress > 255, 255, decompress)
+		decompress = np.where(decompress < 0, 0, decompress)
 
-	elapsed_time = time.time() - start
+		count = 0
 
-	if VERBOSE: print ("finding_difference:{0}".format(elapsed_time) + "[sec]")
+		if len(file_names) != X_test_shape[1]:
+			print("ERROR：The lengths of filename.txt and images do not match.")
+			print("filename.txt：", len(file_names))
+			print("number of images", X_test_shape[1])
+			exit()
 
-	# 画像の出力
-	decompress = X_hat_no_pad * 255
-	decompress = decompress - difference_first
-
-	decompress = np.where(decompress > 255, 255, decompress)
-	decompress = np.where(decompress < 0, 0, decompress)
-
-	count = 0
-
-	if len(file_names) != X_test_shape[1]:
-		print("ERROR：The lengths of filename.txt and images do not match.")
-		print("filename.txt：", len(file_names))
-		print("number of images", X_test_shape[1])
-		exit()
-
-	for i in range(X_test_shape[0]):
-		for j in range(X_test_shape[1]):
-			tmp_np = decompress[i,j,:,:,:]
-			tmp_np = tmp_np.astype('uint8')
-			tmp_image = Image.fromarray(tmp_np)
-			tmp_image.save(os.path.join(OUTPUT_DIR, file_names[count]))
-			count += 1
+		for i in range(X_test_shape[0]):
+			for j in range(X_test_shape[1]):
+				tmp_np = decompress[i,j,:,:,:]
+				tmp_np = tmp_np.astype('uint8')
+				tmp_image = Image.fromarray(tmp_np)
+				tmp_image.save(os.path.join(OUTPUT_DIR, file_names[count]))
+				count += 1
 
